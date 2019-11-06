b3a0c7bedc9c90cc984ea7c55826996eb90702ba
Camilo Barraza <camilo@flatironsdevelopment.com>
2019-11-05 19:55:13 -0500

    remove update file

--- db-sync/dev-sync-db.js
+++ db-sync/dev-sync-db.js
@@ -1,1 +1,11 @@
-console.log('colo')
+const fs = require('fs');
+const { promisify } = require("util");
+
+const writeFile = promisify(fs.writeFile);
+
+async function main() {
+  await writeFile("tmp/update", "checked out or pulled")
+}
+
+main()
+
