f15a21b699f3b2e97f33a92d7624560eb29c3c84
Camilo Barraza <camilo@flatironsdevelopment.com>
2019-11-05 19:11:13 -0500

    update dev-dump

--- db-sync/dev-dump-db-config.js
+++ db-sync/dev-dump-db-config.js
@@ -4,7 +4,7 @@ const { promisify } = require("util");
 const writeFile = promisify(fs.writeFile);
 
 async function main() {
-  await writeFile("tmp/test", "Hey there3!")
+  await writeFile("tmp/test", "Hey there4!")
 }
 
 main()
